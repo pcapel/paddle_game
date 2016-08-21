4dddcff8d288be9621292a56f6e9ed438f6ddcf2
pcapel <insert.available.name@gmail.com>
2016-08-21 10:38:28 -0500

    Player draw and move methods work

--- paddle_game.py
+++ paddle_game.py
@@ -7,218 +7,6 @@ pg.init()
 
 screen = pg.display.set_mode((600, 500))
 
-
-done = False
-
-player_color = (150, 0, 150)
-comp_color = (255, 255, 255)
-
-player_paddle_width = 75
-player_paddle_height = 20
-player_x = 175
-player_y = screen.get_height() - player_paddle_height
-
-comp_width = 15
-comp_height = 15
-comp_x = 150
-comp_y = 150
-
-clock = pg.time.Clock()
-
-x_dir = True
-y_dir = True
-
-font = pg.font.Font(None, 25)
-
-strike_counter = 0
-
-life_count = 3
-
-
-def draw_player(paddle_width=75,
-paddle_height=20,
-flash=False, flash_color=(255,0,0)):
-
-    return pg.draw.rect(screen,
-            player_color,
-            pg.Rect(player_x,
-                player_y,
-                paddle_width,
-                paddle_height))
-
-def draw_ball(size=15):
-    return pg.draw.rect(screen,
-            comp_color,
-            pg.Rect(comp_x,
-                comp_y,
-                size,
-                size))
-
-while not done:
-        for event in pg.event.get():
-                if event.type == pg.QUIT:
-                        done = True
-
-        pressed = pg.key.get_pressed()
-        print pressed
-
-        if (pressed[pg.K_LEFT]
-        and player_x > 0):
-            player_x -= 5
-
-        if (pressed[pg.K_RIGHT]
-        and player_x < screen.get_width()
-                    - player_paddle_width):
-            player_x += 5
-
-
-
-        screen.fill((0, 0, 0))
-        p = draw_player()
-        c = draw_ball()
-
-        if p.colliderect(c):
-            print "collide"
-
-        # paddle_x_zone = (player_x + 1,
-        #                 player_x + player_paddle_width + 1)
-        #
-        # comp_in_zone = (comp_x > paddle_x_zone[0]
-        #                 and comp_x < paddle_x_zone[1])
-        #
-        # comp_in_contact = (comp_in_zone
-        #                 and comp_y == screen.get_height()
-        #                             - player_paddle_height
-        #                             - comp_height)
-        comp_below_contact = (comp_y > screen.get_height()
-                                         - player_paddle_height
-                                         - comp_height)
-        #
-        if p.colliderect(c) and comp_below_contact:
-             y_dir = not y_dir
-             x_dir = not x_dir
-             strike_counter += 1
-        elif p.colliderect(c) and not comp_below_contact:
-            y_dir = not y_dir
-        # elif (comp_in_zone
-        # and comp_below_contact):
-        #     x_dir = not x_dir
-
-        score_text = font.render("Current Score: %d"%strike_counter,
-                    True,
-                    (0, 255,255))
-
-        lives_text = font.render("Lives Remaining: %d"%life_count,
-                    True,
-                    (0,255,255))
-
-        screen.blit(lives_text,
-        (15, 15))
-        screen.blit(score_text,
-        (screen.get_width() - (score_text.get_width() + 15), 15))
-
-        if (x_dir
-        and y_dir):
-            comp_x += 5
-            comp_y += 5
-        elif (not x_dir
-        and not y_dir):
-            comp_x -= 5
-            comp_y -= 5
-        elif (x_dir
-        and not y_dir):
-            comp_x += 5
-            comp_y -= 5
-        elif (not x_dir
-        and y_dir):
-            comp_x -= 5
-            comp_y += 5
-
-        if comp_x >= screen.get_width() - 15:
-            x_dir = not x_dir
-        elif comp_x <= 0:
-            x_dir = not x_dir
-
-        if comp_y >= screen.get_height() - 15:
-            y_dir = not y_dir
-        elif comp_y <= 0:
-            y_dir = not y_dir
-
-        pg.display.flip()
-        clock.tick(60)
-
-
-class Player(pg.draw):
-    """
-    the player class will represent the paddle that the player controls
-    it will have methods associated with controlling power ups, actions,
-    and the like
-    """
-    def __init__(self, *args, **kwargs):
-        #define initial state and pass to GameState
-        self.x_pos = None
-        self.y_pos = None
-        self.dir = None
-        self.speed = None
-
-    def add_aura(size=(20,20), color=(150,150,150), is_flash=False):
-        """
-        adds aura to player paddle, size is a tuple representing the (x,y) value
-        of the rectangle that is used to draw the ellipse, probably use Rect.clamp()
-        to keep it tied to player paddle
-        """
-        pass
-
-    def move_right():
-        self.x_pos += self.speed
-        return None
-
-    def move_left():
-        self.x_pos -= self.speed
-        return None
-
-    def change_speed(value=5):
-        pass
-
-    def change_color(new_color=(0,155,155)):
-        """
-        change player color to passed value
-        """
-        pass
-
-    def player_flash(color=None, period=1):
-        """
-        a method to make the player paddle flash certain colors based on
-        in game criteria
-        """
-        pass
-
-    def update_state(val_name=None, with_val=None):
-        """
-        use the GameState update method to update values in the game state
-        """
-        pass
-
-    def _draw(paddle_width=75, paddle_height=20):
-
-        return pg.draw.rect(screen,
-                player_color,
-                pg.Rect(player_x,
-                    player_y,
-                    paddle_width,
-                    paddle_height))
-
-
-class Ball(Player):
-    """
-    the ball class is the game ball, and will have methods associated with
-    collision, such as tracking position and what type of surface it has collided
-    with, as well as any relevant power up info
-    """
-    def __init__(self, *args, **kwargs):
-        #define initial vectors and position then pass to GameState
-        pass
-
 class GameState():
     """
     the game state class is a modelling class that tracks all the changes to the
@@ -284,3 +72,216 @@ class GameState():
         defaults to 5 seconds, may not work right
         """
         pass
+
+
+class Player():
+    """
+    the player class will represent the paddle that the player controls
+    it will have methods associated with controlling power ups, actions,
+    and the like
+    """
+    def __init__(self, *args, **kwargs):
+        #define initial state and pass to GameState
+        self.x_pos = 175
+        self.y_pos = screen.get_height() - player_paddle_height
+        self.dir = None
+        self.speed = 5
+
+    def add_aura(size=(20,20), color=(150,150,150), is_flash=False):
+        """
+        adds aura to player paddle, size is a tuple representing the (x,y) value
+        of the rectangle that is used to draw the ellipse, probably use Rect.clamp()
+        to keep it tied to player paddle
+        """
+        pass
+
+    def move_right(self):
+        self.x_pos += self.speed
+        return None
+
+    def move_left(self):
+        self.x_pos -= self.speed
+        return None
+
+    def change_speed(self, value=5):
+        pass
+
+    def change_color(self, new_color=(0,155,155)):
+        """
+        change player color to passed value
+        """
+        pass
+
+    def player_flash(self, color=None, period=1):
+        """
+        a method to make the player paddle flash certain colors based on
+        in game criteria
+        """
+        pass
+
+    def update_state(self, val_name=None, with_val=None):
+        """
+        use the GameState update method to update values in the game state
+        """
+        pass
+
+    def get_x(self):
+        return self.x_pos
+
+    def _draw(self, paddle_width=75, paddle_height=20, color=(0,255,0)):
+        return pg.draw.rect(screen,
+                color,
+                pg.Rect(self.x_pos,
+                    self.y_pos,
+                    paddle_width,
+                    paddle_height))
+
+class Ball(Player):
+    """
+    the ball class is the game ball, and will have methods associated with
+    collision, such as tracking position and what type of surface it has collided
+    with, as well as any relevant power up info
+    """
+    def __init__(self, *args, **kwargs):
+        #define initial vectors and position then pass to GameState
+        pass
+
+
+
+
+
+done = False
+
+player_color = (150, 0, 150)
+comp_color = (255, 255, 255)
+
+player_paddle_width = 75
+player_paddle_height = 20
+player_x = 175
+player_y = screen.get_height() - player_paddle_height
+
+comp_width = 15
+comp_height = 15
+comp_x = 150
+comp_y = 150
+
+clock = pg.time.Clock()
+
+x_dir = True
+y_dir = True
+
+font = pg.font.Font(None, 25)
+
+strike_counter = 0
+
+life_count = 3
+
+_player = Player()
+
+def draw_player(paddle_width=75,
+paddle_height=20):
+
+    return pg.draw.rect(screen,
+            player_color,
+            pg.Rect(player_x,
+                player_y,
+                paddle_width,
+                paddle_height))
+
+def draw_ball(size=15):
+    return pg.draw.rect(screen,
+            comp_color,
+            pg.Rect(comp_x,
+                comp_y,
+                size,
+                size))
+
+
+#game loop
+while not done:
+        for event in pg.event.get():
+                if event.type == pg.QUIT:
+                        done = True
+
+        pressed = pg.key.get_pressed()
+
+        if (pressed[pg.K_LEFT]
+        and _player.get_x() > 0):
+            _player.move_left()
+
+        if (pressed[pg.K_RIGHT]
+        and _player.get_x() < screen.get_width()
+                    - player_paddle_width):
+            _player.move_right()
+
+        screen.fill((0, 0, 0))
+        _player._draw()
+        #p = draw_player()
+        #c = draw_ball()
+
+        # paddle_x_zone = (player_x + 1,
+        #                 player_x + player_paddle_width + 1)
+        #
+        # comp_in_zone = (comp_x > paddle_x_zone[0]
+        #                 and comp_x < paddle_x_zone[1])
+        #
+        # comp_in_contact = (comp_in_zone
+        #                 and comp_y == screen.get_height()
+        #                             - player_paddle_height
+        #                             - comp_height)
+        comp_below_contact = (comp_y > screen.get_height()
+                                         - player_paddle_height
+                                         - comp_height)
+        #
+        # if p.colliderect(c) and comp_below_contact:
+        #      y_dir = not y_dir
+        #      x_dir = not x_dir
+        #      strike_counter += 1
+        # elif p.colliderect(c) and not comp_below_contact:
+        #     y_dir = not y_dir
+        # elif (comp_in_zone
+        # and comp_below_contact):
+        #     x_dir = not x_dir
+
+        score_text = font.render("Current Score: %d"%strike_counter,
+                    True,
+                    (0, 255,255))
+
+        lives_text = font.render("Lives Remaining: %d"%life_count,
+                    True,
+                    (0,255,255))
+
+        screen.blit(lives_text,
+        (15, 15))
+        screen.blit(score_text,
+        (screen.get_width() - (score_text.get_width() + 15), 15))
+
+        if (x_dir
+        and y_dir):
+            comp_x += 5
+            comp_y += 5
+        elif (not x_dir
+        and not y_dir):
+            comp_x -= 5
+            comp_y -= 5
+        elif (x_dir
+        and not y_dir):
+            comp_x += 5
+            comp_y -= 5
+        elif (not x_dir
+        and y_dir):
+            comp_x -= 5
+            comp_y += 5
+
+        if comp_x >= screen.get_width() - 15:
+            x_dir = not x_dir
+        elif comp_x <= 0:
+            x_dir = not x_dir
+
+        if comp_y >= screen.get_height() - 15:
+            y_dir = not y_dir
+        elif comp_y <= 0:
+            y_dir = not y_dir
+
+        pg.display.flip()
+        clock.tick(60)
